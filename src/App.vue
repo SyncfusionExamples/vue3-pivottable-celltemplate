7a4a920096803d64ecb7048f30a216017bc7fa85
Hari Venkatesh <harivenkateshe@syncfusion.com>
2021-08-10 18:57:47 +0530

    Corrections made w.r.t generic version reference.

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,14 @@
   import { PivotViewComponent } from "@syncfusion/ej2-vue-pivotview";
   import { createApp } from 'vue/dist/vue.esm-bundler';
   import "../node_modules/@syncfusion/ej2-base/styles/material.css";
-  import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
-  import "../node_modules/@syncfusion/ej2-calendars/styles/material.css";
-  import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
   import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
-  import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  import "../node_modules/@syncfusion/ej2-buttons/styles/material.css";
+  import "../node_modules/@syncfusion/ej2-dropdowns/styles/material.css";
+  import "../node_modules/@syncfusion/ej2-lists/styles/material.css";
   import "../node_modules/@syncfusion/ej2-popups/styles/material.css";
-  import "../node_modules/@syncfusion/ej2-pivotview/styles/material.css";
-  import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
+  import "../node_modules/@syncfusion/ej2-navigations/styles/material.css";
+  import "../node_modules/@syncfusion/ej2-grids/styles/material.css";
+  import "../node_modules/@syncfusion/ej2-vue-pivotview/styles/material.css";
 
   const app = createApp();
 
